8b2977e3edec1e5739753821cfbc87da769f599a
smolgeorgie <ilona.van.oosbree@hva.nl>
2023-12-14 14:12:40 +0100

    first 5

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,46 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+// JUMP HANDLER
+let interactionJump = document.querySelector('a:nth-of-type(12)')
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interactionJump.addEventListener('click', jumpHandler)
+interactionJump.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+  interactionJump.classList.toggle('jump')
 }
+
+
+// SHAKE HANDLER
+let interactionShake = document.querySelector('a:nth-of-type(1)')
+
+interactionShake.addEventListener('click', shakeHandler)
+interactionShake.addEventListener('shake', shakeHandler)
+
+function shakeHandler() {
+  interactionShake.classList.toggle('shake')
+}
+
+// PULSE SCALE
+const button = document.querySelector('a:nth-of-type(6)');
+const initialSize = button.offsetWidth;
+const animationDuration = 1000; // 1 second
+const scaleIncrement = 0.3;
+
+function startPulsing() {
+  let scaleFactor = 1;
+
+  function animate() {
+    button.style.transform = `scale(${scaleFactor})`;
+
+    if (scaleFactor === 1) {
+      scaleFactor -= scaleIncrement;
+    } else if (scaleFactor === 0) {
+      scaleFactor += scaleIncrement;
+    }
+
+    setTimeout(animate, animationDuration / 2);
+  }
+
+  animate();
+}
+
+button.addEventListener('click', startPulsing);
